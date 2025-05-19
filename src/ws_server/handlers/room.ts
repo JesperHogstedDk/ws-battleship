a057db586e679d305549e538f9b9ad660ef5f1be
Jesper Høgsted <81517227+JesperHogstedDk@users.noreply.github.com>
2025-05-19 12:37:49 +0200

    feat: send update_room from all rooms to all players

--- src/ws_server/handlers/room.ts
+++ src/ws_server/handlers/room.ts
@@ -2,34 +2,33 @@ import type { WebSocket } from "ws";
 import { ClientSession, Room } from "../../types/types.js";
 
 export const updateRoom = (ws: WebSocket, rooms: Room[], clients: ClientSession[]) => {
-    const roomsWithOneUser: [] = [];
+
+    const roomsWithOneUser: {}[] = [];
     if (rooms.length > 0) {
         rooms.forEach(room => {
             console.log('room: ', room);
             if (room.roomUsers.length === 1) {
                 room.roomUsers.forEach(user => {
-                    const updateRoomData = [
-                        {
-                            roomId: room.roomId,
-                            roomUsers:
-                                [
-                                    {
-                                        name: `${user.name}'s room! at index ${user.index}`,
-                                        index: user.index,
-                                    }
-                                ],
-                        },
-                    ];
-                    // roomsWithOneUser.push(updateRoomData)
-                    clients.forEach(client => {
-                        const updateRoom = JSON.stringify({ id: 0, type: 'update_room', data: JSON.stringify(updateRoomData) });
-                        console.log('updateRoom: ', updateRoom);
-                        client.ws.send(updateRoom);
-                    });
+                    const updateRoomData =
+                    {
+                        roomId: room.roomId,
+                        roomUsers:
+                            [
+                                {
+                                    name: `${user.name}'s room! at index ${user.index}`,
+                                    index: user.index,
+                                }
+                            ],
+                    };
+                    roomsWithOneUser.push(updateRoomData);
 
                 });
-
             }
+        });
+        clients.forEach(client => {
+            const updateRoom = JSON.stringify({ id: 0, type: 'update_room', data: JSON.stringify(roomsWithOneUser) });
+            console.log('updateRoom: ', updateRoom);
+            client.ws.send(updateRoom);
         });
     } else {
         clients.forEach(client => {
