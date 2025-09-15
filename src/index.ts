e92012b940e30508aa0e77fc21475839546ebcd8
Jesper Høgsted <81517227+JesperHogstedDk@users.noreply.github.com>
2025-09-15 17:25:43 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { httpServer } from "./http_server/index.js";
 import { startWebSockedServer } from "./ws_server/index.js";
 
 const HTTP_PORT = 10000;
-const WS_PORT = 13000;
+const WS_PORT = 3000;
 
 console.log(`Start static http serve on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
